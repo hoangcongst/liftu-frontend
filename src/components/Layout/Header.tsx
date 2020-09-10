b26b56e5a968046fc56f0d373f1303860f732d71
conght <conght@funtap.vn>
2020-09-11 01:03:29 +0700

    add btn login and register

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -8,6 +8,7 @@ import { toggleSetting } from '../../store/actions/actions';
 
 import ToggleFullscreen from '../Common/ToggleFullscreen';
 import HeaderSearch from './HeaderSearch';
+import AuthHelper from '../../helpers/auth.helper';
 type HeaderProps = {
     toggleSetting: typeof toggleSetting,
     expired: string,
@@ -63,6 +64,82 @@ class Header extends Component<HeaderProps> {
         });
     };
 
+    loginBtnOrToggleUserMenu = () => {
+        if (AuthHelper.isAuthenticated(this.props.expired)) {
+            return <>
+                { /* START Alert menu */}
+                <UncontrolledDropdown nav inNavbar className="dropdown-list">
+                    <DropdownToggle nav className="dropdown-toggle-nocaret">
+                        <em className="icon-bell"></em>
+                        <span className="badge badge-danger">11</span>
+                    </DropdownToggle>
+                    { /* START Dropdown menu */}
+                    <DropdownMenu right className="dropdown-menu-right animated flipInX">
+                        <DropdownItem>
+                            { /* START list group */}
+                            <ListGroup>
+                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
+                                    <div className="media">
+                                        <div className="align-self-start mr-2">
+                                            <em className="fab fa-twitter fa-2x text-info"></em>
+                                        </div>
+                                        <div className="media-body">
+                                            <p className="m-0">New followers</p>
+                                            <p className="m-0 text-muted text-sm">1 new follower</p>
+                                        </div>
+                                    </div>
+                                </ListGroupItem>
+                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
+                                    <div className="media">
+                                        <div className="align-self-start mr-2">
+                                            <em className="fa fa-envelope fa-2x text-warning"></em>
+                                        </div>
+                                        <div className="media-body">
+                                            <p className="m-0">New e-mails</p>
+                                            <p className="m-0 text-muted text-sm">You have 10 new emails</p>
+                                        </div>
+                                    </div>
+                                </ListGroupItem>
+                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
+                                    <div className="media">
+                                        <div className="align-self-start mr-2">
+                                            <em className="fa fa-tasks fa-2x text-success"></em>
+                                        </div>
+                                        <div className="media-body">
+                                            <p className="m-0">Pending Tasks</p>
+                                            <p className="m-0 text-muted text-sm">11 pending task</p>
+                                        </div>
+                                    </div>
+                                </ListGroupItem>
+                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
+                                    <span className="d-flex align-items-center">
+                                        <span className="text-sm">More notifications</span>
+                                        <span className="badge badge-danger ml-auto">14</span>
+                                    </span>
+                                </ListGroupItem>
+                            </ListGroup>
+                            { /* END list group */}
+                        </DropdownItem>
+                    </DropdownMenu>
+                    { /* END Dropdown menu */}
+                </UncontrolledDropdown>
+                { /* END Alert menu */}
+                { /* START Offsidebar button */}
+                <li className="nav-item">
+                    <a className="nav-link" href="" onClick={this.toggleOffsidebar}>
+                        <em className="icon-notebook"></em>
+                    </a>
+                </li>
+            </>
+        }
+        else {
+            return <div role="group" className="btn-group" style={{alignItems: 'center', paddingRight: '8px'}}>
+                <Link to="/auth/login" className="text-muted"><button className="btn btn-pill-left btn-success btn-sm">Login</button></Link>
+                <Link to="/auth/register" className="text-muted"><button className="btn btn-pill-right btn-danger btn-sm">Register</button></Link>
+            </div>
+        }
+    }
+
     render() {
         return (
             <header className="topnavbar-wrapper">
@@ -72,15 +149,15 @@ class Header extends Component<HeaderProps> {
                     <div className="navbar-header">
                         <a className="navbar-brand" href="/">
                             <div className="brand-logo">
-                                <img className="img-fluid" src="/img/logo.png" alt="App Logo" />
+                                <img className="img-fluid" src="/img/logo.png" alt="LiftU" />
                             </div>
                             <div className="brand-logo-collapsed">
-                                <img className="img-fluid" src="/img/logo-single.png" alt="App Logo" />
+                                <img className="img-fluid" src="/img/logo-single.png" alt="LiftU" />
                             </div>
                         </a>
                     </div>
                     { /* END navbar header */}
-
+                    
                     { /* START Left navbar */}
                     <ul className="navbar-nav mr-auto flex-row">
                         <li className="nav-item">
@@ -122,71 +199,7 @@ class Header extends Component<HeaderProps> {
                             <ToggleFullscreen className="nav-link" />
                         </li>
                         {
-                            this.props.expired && <>
-                                { /* START Alert menu */}
-                                <UncontrolledDropdown nav inNavbar className="dropdown-list">
-                                    <DropdownToggle nav className="dropdown-toggle-nocaret">
-                                        <em className="icon-bell"></em>
-                                        <span className="badge badge-danger">11</span>
-                                    </DropdownToggle>
-                                    { /* START Dropdown menu */}
-                                    <DropdownMenu right className="dropdown-menu-right animated flipInX">
-                                        <DropdownItem>
-                                            { /* START list group */}
-                                            <ListGroup>
-                                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
-                                                    <div className="media">
-                                                        <div className="align-self-start mr-2">
-                                                            <em className="fab fa-twitter fa-2x text-info"></em>
-                                                        </div>
-                                                        <div className="media-body">
-                                                            <p className="m-0">New followers</p>
-                                                            <p className="m-0 text-muted text-sm">1 new follower</p>
-                                                        </div>
-                                                    </div>
-                                                </ListGroupItem>
-                                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
-                                                    <div className="media">
-                                                        <div className="align-self-start mr-2">
-                                                            <em className="fa fa-envelope fa-2x text-warning"></em>
-                                                        </div>
-                                                        <div className="media-body">
-                                                            <p className="m-0">New e-mails</p>
-                                                            <p className="m-0 text-muted text-sm">You have 10 new emails</p>
-                                                        </div>
-                                                    </div>
-                                                </ListGroupItem>
-                                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
-                                                    <div className="media">
-                                                        <div className="align-self-start mr-2">
-                                                            <em className="fa fa-tasks fa-2x text-success"></em>
-                                                        </div>
-                                                        <div className="media-body">
-                                                            <p className="m-0">Pending Tasks</p>
-                                                            <p className="m-0 text-muted text-sm">11 pending task</p>
-                                                        </div>
-                                                    </div>
-                                                </ListGroupItem>
-                                                <ListGroupItem action tag="a" href="" onClick={e => e.preventDefault()}>
-                                                    <span className="d-flex align-items-center">
-                                                        <span className="text-sm">More notifications</span>
-                                                        <span className="badge badge-danger ml-auto">14</span>
-                                                    </span>
-                                                </ListGroupItem>
-                                            </ListGroup>
-                                            { /* END list group */}
-                                        </DropdownItem>
-                                    </DropdownMenu>
-                                    { /* END Dropdown menu */}
-                                </UncontrolledDropdown>
-                                { /* END Alert menu */}
-                                { /* START Offsidebar button */}
-                                <li className="nav-item">
-                                    <a className="nav-link" href="" onClick={this.toggleOffsidebar}>
-                                        <em className="icon-notebook"></em>
-                                    </a>
-                                </li>
-                            </>
+                            this.loginBtnOrToggleUserMenu()
                         }
                         { /* END Offsidebar menu */}
                     </ul>
